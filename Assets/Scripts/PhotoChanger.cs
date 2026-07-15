ddced78809d791faa4365b279fd922c5ca54ca2f
AnDey950404 <yixan0303@gmail.com>
2026-07-15 12:13:08 -0500

    修改入口一到入口二正常使用

--- Assets/Scripts/PhotoChanger.cs
+++ Assets/Scripts/PhotoChanger.cs
@@ -3,12 +3,35 @@ using UnityEngine.UI;
 
 public class PhotoChanger : MonoBehaviour
 {
+    [Header("照片顯示")]
     public Image background;
 
-    public Sprite photo;
+    [Header("地點照片")]
+    public Sprite entrancePhoto;
+    public Sprite templePhoto;
 
-    public void ChangePhoto()
+    [Header("各地點按鈕")]
+    public GameObject entranceHotspots;
+    public GameObject templeHotspots;
+
+    private void Start()
     {
-        background.sprite = photo;
+        ShowEntrance();
+    }
+
+    public void ShowEntrance()
+    {
+        background.sprite = entrancePhoto;
+
+        entranceHotspots.SetActive(true);
+        templeHotspots.SetActive(false);
+    }
+
+    public void ShowTemple()
+    {
+        background.sprite = templePhoto;
+
+        entranceHotspots.SetActive(false);
+        templeHotspots.SetActive(true);
     }
 }
